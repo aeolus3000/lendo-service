909066284c620e57c472460e1b2a7761e2cd1532
kopps <simon.kopp@thi.de>
2021-03-24 23:22:21 +0100

    add config prefix to banking api conf

--- middleware/banking.go
+++ middleware/banking.go
@@ -18,7 +18,7 @@ var (
 func init() {
 	config := configuration.NewDefaultConfiguration()
 	bankingConfiguration := banking.Configuration{}
-	confErr := config.Process(configBankingPrefix, &bankingConfiguration)
+	confErr := config.Process(configPrefix + configBankingPrefix, &bankingConfiguration)
 	if confErr != nil {
 		log.Fatal(confErr)
 	}
